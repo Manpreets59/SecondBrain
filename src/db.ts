8ba937496bb74887944d1a77762f4083d52030ed
Manpreet <manpreets95828@gmail.com>
2025-04-14 00:31:55 +0530

    Signup Schema

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,11 @@
 // create model and Schema over here
+import mongoose, {model, Schema} from "mongoose";
+
+mongoose.connect("mongodb://Localhost:27017/brain")
+
+const UserSchema = new Schema({
+  username: {type: String, unique: true},
+  password: String
+})
+
+export const UserModel = model("User",UserSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,46 @@
 import express from "express";
 import mongoose from "mongoose";
 import Jwt from "jsonwebtoken";
+import { UserModel } from "./db";
 
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup", (req, res) = {
+
+app.post("/api/v1/signup", async(req, res) => {
+  //Zod validation, // hash the password 
+  const username = req.body.username;
+  const password = req.body.password;
+
+  await UserModel.create({
+    username: username, 
+    password: password
+  })
+  res.json({
+    message: "user sign up success"
+  })
+})
+
+app.post("/api/v1/signin", (req, res) => {
+  const username = req.body.username;
+})
+
+app.post("/api/v1/content", (req, res) => {
 
 })
 
-app.post("/api/v1/signin", (req, res) = {
+app.get("/api/v1/content", (req, res) => {
 
 })
 
-app.post("/api/v1/content", (req, res) = {
+app.delete("/api/v1/content", (req, res) => {
 
 })
 
-app.get("/api/v1/content", (req, res) = {
+app.post("/api/v1/brain/share", (req, res) => {
 
 })
 
-app.delete("/api/v1/content", (req, res) = {
-
-})
-
-app.post("/api/v1/brain/share", (req, res) = {
-
-})
-
-app.get("/api/v1/brain/:shareLink", (req, res) = {
+app.get("/api/v1/brain/:shareLink", (req, res) => {
 
 })
