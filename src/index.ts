958a3d3ed2c5aa61b63b49e51138e9822d69d8fb
Manpreet <manpreets95828@gmail.com>
2025-04-14 22:27:42 +0530

    signup and signin endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
-import Jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { UserModel } from "./db";
+
+const JWT_PASSWORD = "!123123";
 
 const app = express();
 app.use(express.json());
+
+app.listen(3000);
 
 
 app.post("/api/v1/signup", async(req, res) => {
@@ -12,17 +16,38 @@ app.post("/api/v1/signup", async(req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
+  try{
   await UserModel.create({
     username: username, 
     password: password
   })
   res.json({
     message: "user sign up success"
-  })
+  })}catch(e){
+    res.status(411).json({
+      message: "user already exists"
+    })
+  }
 })
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
   const username = req.body.username;
+  const password = req.body.password;
+
+  const existingUser = await UserModel.findOne({username, password})
+  if(existingUser){
+    const token = jwt.sign({
+      id: existingUser._id
+    }, JWT_PASSWORD)
+
+    res.json({
+      token
+    })
+  }else{
+    res.status(403).json({
+      message: "Incorrect credentials"
+    })
+  }
 })
 
 app.post("/api/v1/content", (req, res) => {
