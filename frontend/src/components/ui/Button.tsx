199407bc7705c13100e149cf8b2ef52d86db759d
Manpreet <manpreets95828@gmail.com>
2025-04-26 23:52:00 +0530

    Frontend changes

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,13 +1,24 @@
+import { ReactElement } from "react";
+
+type Variants = "primary" | "secondary"
 interface ButtonProps{
-  variant: "primary" | "secondary" ;
+  variant: Variants ;
   size: "sm" | "md" | "lg";
   text: string;
-  startIcon?: any;
-  endIcon?: any;
+  startIcon?: ReactElement;
+  endIcon?: ReactElement;
   onClick: () => void;
 }
 
+const variantStyles: {[key: Variants]: string} = {
+  "default": "bg-purple-600 text-white",
+
 export const Button = (props: ButtonProps) => {
+  if(props.variant === "primary"){
+    if(props.size === "lg"){
+
+    }
+  }
   return <button></button>
 }
 <Button endIcon={"+"} variant = "primary" size = "md" onClick= {() => {}} text= {"asd"} startIcon= {"-"} />
