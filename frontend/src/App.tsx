d2f01d8ffb6909d03a38d1823dd06aeaeb864734
Manpreet <manpreets95828@gmail.com>
2025-04-27 17:24:18 +0530

    Button Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { Button } from './components/ui/Button'
+import { PlusIcon } from './icons/PlusIcon'
 
 function App() {
 
   return (
     <>
+    <Button startIcon={<PlusIcon/>}></Button>
     <Button variant={'primary'} text={'Hello'}/>
+    <Button variant={'secondary'} text={'Hello'}/>
     </>
   )
 }
