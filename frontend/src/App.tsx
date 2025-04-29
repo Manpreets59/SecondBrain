31a814d41ff788d6ac5fe50ca7a5e01f05c8a480
Manpreet <manpreets95828@gmail.com>
2025-04-29 23:27:57 +0530

    initial commmit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
 import { Button } from './components/ui/Button'
 import { PlusIcon } from './icons/PlusIcon'
+import { ShareIcon } from './icons/ShareIcon'
 
 function App() {
 
   return (
     <>
-    <Button startIcon={<PlusIcon/>}></Button>
-    <Button variant={'primary'} text={'Hello'}/>
-    <Button variant={'secondary'} text={'Hello'}/>
+    <Button startIcon={<PlusIcon  size = {"lg"}/>}
+            endIcon={<ShareIcon sie = {"lg"}}/></Button>
+    {/* <Button variant={'primary'} text={'Hello'}/>
+    <Button variant={'secondary'} text={'Hello'}/> */}
     </>
   )
 }
